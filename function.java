09963f6271ce113c82c89360021fd8a1c9c9b0f7
Souvik Roy <roy.souvik000@gmail.com>
2016-09-19 00:09:51 +0530

    Create function.java

---
+++ function.java
@@ -0,0 +1,30 @@
+//Calling a function without calling it
+//cerner_2^5_2016
+
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+
+public class CallWithoutCalling {
+
+	 public static class StrangeException extends RuntimeException {
+	        @Override
+	        public void printStackTrace(PrintStream s) {
+	            for (Method m : CallWithoutCalling.class.getMethods()) {
+	                if ("main".equals(m.getName())) continue;
+	                try {
+	                    m.invoke(null);
+	                } catch (Exception e) {
+	                    //e.printStackTrace();
+	                }
+	            }
+	        }
+	    }
+
+	    public static void secretMethodNotCalledInMain() {
+	        System.out.println("First Hand Foundation !");
+	    }
+
+	    public static void main(String[] args) {
+	        throw new StrangeException();
+	    }
+}
