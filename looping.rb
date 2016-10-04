bcf9b89afcdc57953ae860ee76f71aaaff8dc166
Souvik Roy <roy.souvik000@gmail.com>
2016-10-04 13:54:12 +0530

    Add files via upload

---
+++ looping.rb
@@ -0,0 +1,23 @@
+#Loop without 'looping'. without while, for, do while, foreach or goto. without recursion of function.  
+
+#cerner_2^5_2016
+
+def method_missing(meth,*args)
+
+  puts 'Loop'
+
+  send(meth.next)
+
+end
+
+
+
+def also
+
+  puts "++Stop++"
+
+end
+
+
+
+ahem
